a9c1135a380ec30e6ff1c42f7136b6a64483715e
anthony mestdach <anthony.mestdach@student.odisee.be>
2016-11-09 17:29:57 +0100

    update

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,45 +1,19 @@
 package hello;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.thymeleaf.spring.support.Layout;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-import java.util.function.Function;
-
-@Controller
+@RestController
 public class GreetingController {
 
-    private Log log = LogFactory.getLog(GreetingController.class);
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
 
-    List<String> persons = Arrays.asList("Walter White", "Heisenberg", "Jesse Pinkman", "Saul Goodman", "Gustavo Fringe");
-
-    Function<List<String>, String> random = (List<String> list) -> list.get(new Random().nextInt(list.size() - 1));
-
-    @Layout("layouts/logged_in")
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
-        log.debug("In greeting()");
-        model.addAttribute("name", name);
-        return "greeting";
+    @RequestMapping("/hello")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
     }
-
-    @Layout("layouts/logged_in")
-    @RequestMapping("/randomgreeting")
-    public String whoAmI(Model model) {
-        final String randomName = random.apply(persons);
-        model.addAttribute("name",
-                persons
-                        .stream()
-                        .filter(person -> person.equals(randomName))
-                        .findFirst().get());
-        return "greeting";
-    }
-
 }
