b5915475cfd499dc90c051cde5211a528f0279c9
anthony mestdach <anthony.mestdach@student.odisee.be>
2016-11-13 15:56:40 +0100

    add origin

--- src/main/java/restService/Greeting.java
+++ src/main/java/restService/Greeting.java
@@ -1,4 +1,4 @@
-package hello;
+package restService;
 
 public class Greeting {
 
--- src/main/java/restService/restController.java
+++ src/main/java/restService/restController.java
@@ -1,19 +1,26 @@
-package hello;
+package restService;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.social.facebook.api.*;
+import org.springframework.social.connect.ConnectionRepository;
 
 @RestController
-public class GreetingController {
+public class restController {
 
-    private static final String template = "Hello, %s!";
+	private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/hello")
+    @RequestMapping("/greeting")
+    @CrossOrigin(origins = "https://student-dating-test.herokuapp.com")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
+    
 }
